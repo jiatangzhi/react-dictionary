a1a2f1746e593d6d89b78a2ed9c95f8e59f73994
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-21 23:13:26 +0200

    created the search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"> </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="https://www.jxtangzhi.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Jia Xin Tang Zhi
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/jiatangzhi/react-dictionary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-
