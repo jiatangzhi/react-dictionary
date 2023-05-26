c89796664ab5c7e4c36035045fc55fbd3b1381d8
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-26 02:47:28 +0200

    fixed synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./Synonyms.js";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  <ul className="Synonyms">
-    {props.synonyms.map(function (synonym, index) {
-      return <li key={index}>{synonym}</li>;
-    })}
-  </ul>;
+  return (
+    <ul className="Synonyms">
+      {props.synonyms.map(function (synonym, index) {
+        return <li key={index}>{synonym}</li>;
+      })}
+    </ul>
+  );
 }
